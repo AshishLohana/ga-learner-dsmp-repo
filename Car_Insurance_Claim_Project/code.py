f5b53fb6f722f0e9eea0a1085154855e5ea085bb
AshishLohana <ashishlohana@gmail.com>
2019-05-31 20:15:36 +0530

    Code file created.

---
+++ Car_Insurance_Claim_Project/code.py
@@ -0,0 +1,95 @@
+# --------------
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+# Code starts here
+df=pd.read_csv(path)
+cols=['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']
+df[cols] = df[cols].replace({'\$': '', ',': ''}, regex=True)
+print(df.head(5))
+X=df.iloc[:,:-1]
+y=df.iloc[:,-1]
+count=y.value_counts()
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=6)
+# Code ends here
+
+
+# --------------
+# Code starts here
+cols = ['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']
+X_train[cols] = X_train[cols].apply(pd.to_numeric)
+X_test[cols] = X_test[cols].apply(pd.to_numeric)
+print(X_train.isnull().sum())
+print(X_test.isnull().sum())
+print(X_train.dtypes)
+# Code ends here
+
+
+# --------------
+# Code starts here
+X_train.dropna(subset = ['YOJ','OCCUPATION'],inplace=True)
+X_test.dropna(subset = ['YOJ','OCCUPATION'],inplace=True)
+y_train=y_train[X_train.index]
+y_test=y_test[X_test.index]
+cols=['AGE','CAR_AGE','INCOME','HOME_VAL']
+for i in cols:
+    X_train[cols]=X_train[cols].fillna(X_train[cols].mean())
+    X_test[cols]=X_test[cols].fillna(X_test[cols].mean())
+# Code ends here
+
+
+# --------------
+from sklearn.preprocessing import LabelEncoder
+columns = ["PARENT1","MSTATUS","GENDER","EDUCATION","OCCUPATION","CAR_USE","CAR_TYPE","RED_CAR","REVOKED"]
+
+# Code starts here
+le=LabelEncoder()
+for i in range(0,len(columns)):
+    X_train[columns[i]]=le.fit_transform(X_train[columns[i]].astype(str))
+    X_test[columns[i]]=le.transform(X_test[columns[i]].astype(str))
+# Code ends here
+
+
+
+# --------------
+from sklearn.metrics import precision_score 
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+# code starts here 
+model=LogisticRegression(random_state = 6)
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+score=accuracy_score(y_test,y_pred)
+precision=precision_score(y_test,y_pred)
+print(f"Accuracy_Score: {score}")
+print(f"Precision: {precision}")
+# Code ends here
+
+
+# --------------
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+
+# code starts here
+smote=SMOTE(random_state=9)
+X_train,y_train=smote.fit_sample(X_train,y_train)
+scaler = StandardScaler()
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+# Code ends here
+
+
+# --------------
+# Code Starts here
+model=LogisticRegression()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+score=accuracy_score(y_test,y_pred)
+print(f"Accuracy_Score: {score}")
+# Code ends here
+
+
